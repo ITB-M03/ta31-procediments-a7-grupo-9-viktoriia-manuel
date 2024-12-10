441845244cac98508e96d7c146570cbee719e8de
viktoriia.ponomarenko.7e6 <viktoriia.ponomarenko.7e6@itb.cat>
2024-12-10 12:26:36 +0100

    Exe2 sin test

--- src/main/kotlin/controllers/Exe1/Exe1.kt
+++ src/main/kotlin/controllers/Exe1/Exe1.kt
@@ -32,6 +32,7 @@ fun pedirNumero(msg: String, scanner: Scanner): Int {
     return numero
 }
 
+
 /**
  * Convierte un número entero en su equivalente en números romanos.
  * La conversión se realiza utilizando una lista de valores romanos predefinidos. Se comienza con el valor más alto
