2bfc8a22177736794b1e3b96ef212d0efd679307
Manuel Felix <manuel.felix.7e8@itb.cat>
2024-12-11 10:05:56 +0100

    EJERCICIO 5 ACABADO.

---
+++ src/test/kotlin/controllers/A7_EJ5/EJ5KtTest.kt
@@ -0,0 +1,29 @@
+package controllers.A7_EJ5
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+ class EJ5KtTest {
+
+@Test
+ fun menu() {}
+
+@Test
+ fun manejarOpciones() {}
+
+@Test
+ fun push() {}
+
+@Test
+ fun pop() {}
+
+@Test
+ fun mostrarContenidoPila() {}
+}
+
+
+/*
+Las funciones de este programa no devuelven nada, por lo que no es posible
+hacer plan de pruebas. La única función que devuelve algo es la de abrir scanner,
+pero no es una función que se pueda probar.
+ */
