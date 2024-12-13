63c943c1941b110d27da7c95011f414244578eed
viktoriia.ponomarenko.7e6 <viktoriia.ponomarenko.7e6@itb.cat>
2024-12-13 11:22:55 +0100

    Exe2 Test

--- src/test/kotlin/controllers/Exe2/Exe2KtTest.kt
+++ src/test/kotlin/controllers/Exe2/Exe2KtTest.kt
@@ -5,22 +5,23 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.*
  class Exe2KtTest {
 
+
  @Test
-  fun `roman_convierte_números_pequeños`() {
+  fun roman_convierte_numeros_pequenos() {
    assertEquals("I", roman(1))
    assertEquals("IV", roman(4))
    assertEquals("IX", roman(9))
   }
 
   @Test
-  fun `roman_convierte_números_medianos`() {
+  fun roman_convierte_numeros_medianos() {
    assertEquals("XL", roman(40))
    assertEquals("XC", roman(90))
    assertEquals("CD", roman(400))
   }
 
   @Test
-  fun `roman_convierte_números_grandes`() {
+  fun roman_convierte_numeros_grandes() {
    assertEquals("M", roman(1000))
    assertEquals("MMXXIV", roman(2024))
    assertEquals("MMMCMXCIX", roman(3999))
@@ -29,6 +30,5 @@ import org.junit.jupiter.api.Assertions.*
 /**
  * No puedo probar directamente funciones que usan `Scanner` porque dependen de `System.in`,
  * lo que requiere entrada en tiempo real desde el teclado. Esto no es controlable en pruebas automatizadas.
- *
  * @authorViktoriia
  */
