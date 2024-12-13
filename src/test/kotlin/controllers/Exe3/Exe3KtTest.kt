6131f7ac6d94a2a6327bd9ce82bbc7bbf583f8db
viktoriia.ponomarenko.7e6 <viktoriia.ponomarenko.7e6@itb.cat>
2024-12-13 13:47:07 +0100

    Exe3 Test

--- src/test/kotlin/controllers/Exe3/Exe3KtTest.kt
+++ src/test/kotlin/controllers/Exe3/Exe3KtTest.kt
@@ -1,19 +1,8 @@
-//package controllers.Exe3
-//import org.junit.jupiter.api.Test
-//import kotlin.test.assertEquals
-//
-//class Exe3Tests {
-//
-// @Test
-// fun testvalidarSuperusuari() {
-//  val output = mostrarResultado(true)
-//  assertEquals("Acceso concedido al superusuario.", output)
-// }
-//
-// @Test
-// fun testvalidarSuperusuari2() {
-//  val output = mostrarResultado(false)
-//  assertEquals("Acceso denegado. Has agotado los intentos.", output)
-// }
-//
-//}
+package controllers.Exe3
+
+
+/**
+ * No puedo probar directamente funciones que usan `Scanner` porque dependen de `System.in`,
+ * lo que requiere entrada en tiempo real desde el teclado. Esto no es controlable en pruebas automatizadas.
+ * @authorViktoriia
+ */
